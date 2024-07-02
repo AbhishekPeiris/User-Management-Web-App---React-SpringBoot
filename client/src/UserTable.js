437a24369bb6868ddfe852813232b2f45eba29f8
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-07-02 11:32:45 +0530

    Create and read

--- client/src/UserTable.js
+++ client/src/UserTable.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function UserTable() {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+
+        async function getUsers(){
+
+            try {
+                const response = await axios.get("http://localhost:9090/api/v1/getusers")
+                setUsers(response.data)
+                console.log(response.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getUsers();
+
+    },[])
   return (
     <div className='container'>
         <div className='row tablerow'>
@@ -14,6 +33,15 @@ function UserTable() {
                 <strong><p>Action</p></strong>
             </div>
         </div>
+        {users.map((user) => (
+            <div className='row tablerow' key={user.id}>
+                <div className='col-md-3 border tablecol'>{user.id}</div>
+                <div className='col-md-3 border tablecol'>{user.name}</div>
+                <div className='col-md-3 border tablecol'>
+                    <button className='btn btn-danger'>Delete</button>
+                </div>
+            </div>
+        ))}
     </div>
   )
 }
